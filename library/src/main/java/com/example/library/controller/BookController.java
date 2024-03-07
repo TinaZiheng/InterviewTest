fd6dc4dba87058a10ca99b91f17c3da7cf733b33
DELL15\gear1 <972695238@qq.com>
2024-03-07 16:48:41 +0800

    And add, find, update, delete book's api

--- library/src/main/java/com/example/library/controller/BookController.java
+++ library/src/main/java/com/example/library/controller/BookController.java
@@ -1,5 +1,69 @@
 package com.example.library.controller;
 
-public class BookController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.library.model.Book;
+import com.example.library.repository.BookRepository;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@PostMapping("/addBook")
+	public ResponseEntity<Book> addBook(@RequestBody Book book) {
+		try {
+			Book newBook = bookRepository.saveAndFlush(book);
+			return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/getBook/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable("id") long id) {
+		Optional<Book> tutorialData = bookRepository.findById(id);
+		if (tutorialData.isPresent()) {
+			return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/updateBook/{id}")
+	public ResponseEntity<Book> updateTutorial(@PathVariable("id") long id, @RequestBody Book book) {
+		Optional<Book> bookData = bookRepository.findById(id);
+
+		if (bookData.isPresent()) {
+			Book oldBook = bookData.get();
+			oldBook.setRemark(book.getRemark());
+			return new ResponseEntity<>(bookRepository.save(oldBook), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/delBook/{id}")
+	public ResponseEntity<HttpStatus> deleteBook(@PathVariable("id") long id) {
+		try {
+			bookRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
--- library/src/main/java/com/example/library/model/Book.java
+++ library/src/main/java/com/example/library/model/Book.java
@@ -1,5 +1,61 @@
 package com.example.library.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String remark;
+	
+	public int getId() {
+		return Id;
+	}
+	
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getRemark() {
+		return name;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public Book(int id, String name, String remark) {
+		super();
+		Id = id;
+		this.name = name;
+		this.remark = remark;
+	}
+	
+	public Book(String name, String remark) {
+		this(0, name, remark);
+	}
+
+	public Book() {
+		super();
+	}
 
 }
